9d08c8b5b2c6b7ed9bb32fcff38843e5b3f7da6d
Colin Frankish <colin.frankish@gmail.com>
2014-01-07 19:05:04 +0000

    Methods for header,footer,students added

--- directory.rb
+++ directory.rb
@@ -19,13 +19,22 @@ students = [
 	 "Megan Folsom",
 	 "Berta Gutierrez",
 	]
-	# And then print them
+	def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts "-----------------"
-	students.each do | student |
-		puts student
 	end
 
-	# finally we print the total
-	puts "Overall, we have #{students.length} great students" 
+	def print(names)
+		names.each do | name|
+		puts name
+	end
 
+	def print_footer(names)
+	puts "Overall, we have #{students.length} great students" 
+	end
+
+	# nothing happens till we call the methods
+	print_header
+	print(students)	
+	print_footer(students)
+end
