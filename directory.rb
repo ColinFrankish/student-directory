820cf442059950f69d1d7e621cb10ba672b3a8bd
Colin Frankish <colin.frankish@gmail.com>
2014-01-07 18:50:09 +0000

    Added array and iteration

--- directory.rb
+++ directory.rb
@@ -1,24 +1,31 @@
-student_count = 9
-# first print the list of students.
-puts "The students of my cohort at Makers Academy"
-puts "-----------------------"
-puts "Peter Saxton"
-puts "Nabin Rai"
-puts "Makis Otman"
-puts "Mihai-Liviu Cojocar"
-puts "Biwek Shrestha"
-puts "Audrey Krahmal"
-puts "Kalle Malmiharju"
-puts "Ayaz Goulamabasse"
-puts "Colin Frankish"
-puts "Jorja Tracy Hung"
-puts "Roy Gardiner"
-puts "Abraham Thomas"
-puts "James Hunter"
-puts "Rolando Barbella"
-puts "Megan Folsom"
-puts "Berta Gutierrez"
-# At the end we print the total number of students
-print "Overall, we have #{student_count} great students"
-# print doesn't add new line characters, but puts does! 
+
+# put the students into an array
+
+students = [
+	 "Peter Saxton",
+	 "Nabin Rai",
+	 "Makis Otman",
+	 "Mihai-Liviu Cojocar",
+	 "Biwek Shrestha",
+	 "Audrey Krahmal",
+	 "Kalle Malmiharju",
+	 "Ayaz Goulamabasse",
+	 "Colin Frankish",
+	 "Jorja Tracy Hung",
+	 "Roy Gardiner",
+	 "Abraham Thomas",
+	 "James Hunter",
+	 "Rolando Barbella",
+	 "Megan Folsom",
+	 "Berta Gutierrez",
+	]
+	# And then print them
+	puts "The students of my cohort at Makers Academy"
+	puts "-----------------"
+	students.each do | student |
+		puts student
+	end
+
+	# finally we print the total
+	puts "Overall, we have #{students.length} great students" 
 
