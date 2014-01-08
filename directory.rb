5b0b02461b9724c6e63f175cc931e9f5dd02ddd9
Colin Frankish <colin.frankish@gmail.com>
2014-01-08 18:21:07 +0000

    Array and cohort added

--- directory.rb
+++ directory.rb
@@ -2,39 +2,40 @@
 # put the students into an array
 
 students = [
-	 "Peter Saxton",
-	 "Nabin Rai",
-	 "Makis Otman",
-	 "Mihai-Liviu Cojocar",
-	 "Biwek Shrestha",
-	 "Audrey Krahmal",
-	 "Kalle Malmiharju",
-	 "Ayaz Goulamabasse",
-	 "Colin Frankish",
-	 "Jorja Tracy Hung",
-	 "Roy Gardiner",
-	 "Abraham Thomas",
-	 "James Hunter",
-	 "Rolando Barbella",
-	 "Megan Folsom",
-	 "Berta Gutierrez",
-	]
-	def print_header
+ ["Peter Saxton", :january],
+ ["Nabin Rai", :january],
+ ["Makis Otman", :january],
+ ["Mihai-Liviu Cojocar", :january],
+ ["Biwek Shrestha", :january],
+ ["Audrey Krahmal", :january],
+ ["Kalle Malmiharju", :january],
+ ["Ayaz Goulamabasse", :january],
+ ["Colin Frankish", :january],
+ ["Jorja Tracy Hung", :january],
+ ["Roy Gardiner", :january],
+ ["Abraham Thomas", :january],
+ ["James Hunter", :january],
+ ["Rolando Barbella", :january],
+ ["Megan Folsom", :january],
+ ["Berta Gutierrez", :january],
+]
+def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts "-----------------"
-	end
-
-	def print(names)
-		names.each do | name|
-		puts name
-	end
-
-	def print_footer(names)
-	puts "Overall, we have #{students.length} great students" 
-	end
-
-	# nothing happens till we call the methods
-	print_header
-	print(students)	
-	print_footer(students)
 end
+
+def print(students)
+	students.each do |student|
+		puts "#{student[0]} (#{student[1]} cohort)"
+	end
+end
+
+def print_footer(names)
+	puts "Overall, we have #{students.length} great students" 
+end
+
+# nothing happens till we call the methods
+print_header
+print(students)	
+print_footer(students)
+
