0f721b7785e146d48b797077f43259be37d40949
Colin Frankish <colin.frankish@gmail.com>
2014-01-11 18:23:11 +0000

    Team listing added

--- players.rb
+++ players.rb
@@ -6,14 +6,15 @@ def input_players
 	count = 1
 	until count > 5
 		puts "please enter the first name of a player."
-		name = gets.chomp
+		firstname = gets.chomp
+		players << {:firstname => firstname}
 		puts "And the surname please!"
-		name = gets.chomp
-		players << {:name => name}
+		lastname = gets.chomp
+		players << {:lastname => lastname}
 		count += 1
-		puts "we now have #{players.length} players. "
+		puts "we now have #{players.length/2} players. "
 	end
-
+	players
 end
 
 
@@ -21,9 +22,23 @@ def print_title
 	puts "We are going to list the players in a 5-a-side football team"
 	puts"---------------------------"
 end
+
+def print_team(players)
+		puts "Your team is"
+			players.each do |player|
+			print "Player #{player[:firstname]}, #{player[:lastname]}\n"
+	end
+		
+end
+	
+def print_footer
+	puts "This team looks like it will win the World Cup!"
+		
+	end	
+	
+
 print_title
 players = input_players
-
-
+print_team(players)
 
 
