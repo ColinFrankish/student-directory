b284f42ea90419ee94bb71297d65416b30701c83
Colin Frankish <colin.frankish@gmail.com>
2014-04-17 17:23:44 +0100

    Players listing refactored

--- players.rb
+++ players.rb
@@ -1,22 +1,18 @@
 # try to write program for footy players and team
 
-
+	
+	@players = []
 def input_players
-	players_first = []
-	players_last = []
 	count = 1
 	until count > 5
 		puts "please enter the first name of a player."
 		firstname = gets.chomp
-		players_first << {:firstname => firstname}
 		puts "And the surname please!"
 		lastname = gets.chomp
-		players_last << {:lastname => lastname}
+		@players << {:firstname => firstname,:lastname => lastname}
 		count += 1
-		puts "we now have #{players_first.length} players. "
+		puts "we now have #{@players.length} players. "
 	end
-	players_first
-	players_last
 end
 
 
@@ -25,18 +21,12 @@ def print_title
 	puts"---------------------------"
 end
 
-def print_team(players_first,players_last)
-		
-			players_first.each do |player|
-			print "Player #{player[:firstname]}"
-			players_last.each do |surname|
-			print" #{surname[:lastname]}\n"
-	end
-end
+def print_team	
+			@players.each { |player| puts "Player #{player[:firstname]}" + " " + "#{player[:lastname]}" }
 end	
 
-def Number_of_players(players_first)
-	puts "You have a team of #{players_first.length}"
+def number_of_players
+	puts "You have a team of #{@players.length}"
 end
 
 	
@@ -48,8 +38,8 @@ def print_footer
 
 print_title
 players = input_players
-print_team(players_first,players_last)
-Number_of_players(players_first)
+print_team
+number_of_players
 print_footer
 
 
